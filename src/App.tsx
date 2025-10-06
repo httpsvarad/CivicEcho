5a795b40b20f78e6ba904e5b84639507cb0b3a2c
Varad Manegopale <varad.manegopale28@gmail.com>
2025-10-06 14:57:24 +0530

    logo added

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center gap-2">
-              <img src="./assets/logo2.png" alt="CivicEcho" className="h-8" />
+              <img src="./assets/logo3.png" alt="Sonic AI" className="h-8" />
               {/* <h1 className="text-xl font-bold text-gray-900">
                 CivicEcho
               </h1> */}
